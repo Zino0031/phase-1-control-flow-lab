dda694bfdc95fef132edbd208520c83f5178e9b5
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-07-20 00:55:22 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let answer;
+
+function scuberGreetingForFeet(feet){
+  
+
+      if (feet <= 400) {
+        answer = "This one is on me!";
+      }
+      else if (feet > 400 && feet <= 2000) {
+        answer = "That will be twenty bucks.";  
+      }
+      else if (feet>2000 && feet <= 2500){
+        answer = 'I will gladly take your thirty bucks.';
+      }else if (feet >= 2500){
+        answer = 'No can do.';
+      }
+
+    return answer;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+   return "Ok, sounds good.";
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ 
+  switch (tip){
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default :
+      return 'Bye.';
+  }
 }
